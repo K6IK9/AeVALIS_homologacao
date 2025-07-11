a7fd0060ba800a77acd2fcedea6f4402f59bd2d8
Kaike Matos <kaikematos50@gmail.com>
2025-07-11 19:06:49 -0400

    Ajuste Tela de Perfil

---
+++ avaliacao_docente/templatetags/user_tags.py
@@ -0,0 +1,59 @@
+from django import template
+from rolepermissions.checkers import has_role
+
+register = template.Library()
+
+
+@register.filter
+def get_user_role(user):
+    """
+    Template filter para obter a role do usuário
+    """
+    if has_role(user, "admin"):
+        return "Administrador"
+    elif has_role(user, "coordenador"):
+        return "Coordenador"
+    elif has_role(user, "professor"):
+        return "Professor"
+    elif has_role(user, "aluno"):
+        return "Aluno"
+    else:
+        return "Sem role definida"
+
+
+@register.filter
+def get_user_role_class(user):
+    """
+    Template filter para obter a classe CSS baseada na role do usuário
+    """
+    if has_role(user, "admin"):
+        return "role-admin"
+    elif has_role(user, "coordenador"):
+        return "role-coordenador"
+    elif has_role(user, "professor"):
+        return "role-professor"
+    elif has_role(user, "aluno"):
+        return "role-aluno"
+    else:
+        return "role-sem-role"
+
+
+@register.filter
+def get_user_profile_type(user):
+    """
+    Template filter para obter o tipo de perfil do usuário
+    """
+    if hasattr(user, 'perfil_professor'):
+        return "Professor"
+    elif hasattr(user, 'perfil_aluno'):
+        return "Aluno"
+    else:
+        return "Administrativo"
+
+
+@register.filter
+def has_user_role(user, role):
+    """
+    Template filter para verificar se o usuário tem uma role específica
+    """
+    return has_role(user, role)
