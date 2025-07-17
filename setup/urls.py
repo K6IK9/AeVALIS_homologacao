12e66a6539ccd09edbdbec3debef5fbdfdea6811
Kaike Matos <kaikematos50@gmail.com>
2025-07-16 20:55:58 -0400

    Ajuste em arquivos statics

--- setup/urls.py
+++ setup/urls.py
@@ -1,15 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from avaliacao_docente import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('avaliacao_docente.urls')),
-    path('registro/', views.RegistrarUsuarioView.as_view(), name='registro'),
-    path('accounts/', include('django.contrib.auth.urls')),  # Para autenticação
+    path("admin/", admin.site.urls),
+    path("", include("avaliacao_docente.urls")),
+    path("registro/", views.RegistrarUsuarioView.as_view(), name="registro"),
+    path("accounts/", include("django.contrib.auth.urls")),  # Para autenticação
 ]
 
-
+# Servir arquivos estáticos em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
